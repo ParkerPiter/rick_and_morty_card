49fcc4667554649b2f4ad418411840860c3a429c
Gabriel Alejandro Leal <68304965+ParkerPiter@users.noreply.github.com>
2023-05-22 00:08:13 -0400

    Update data.js image

--- src/data.js
+++ src/data.js
@@ -34,7 +34,7 @@ export const Rick = {
          name: 'unknown',
          url: '',
       },
-      image: 'https://previews.dropbox.com/p/thumb/AB7IEWtq7XlmS6X6XNMzdORTVSdIEsdLI_JEskK0KUTP_QA4ggvFNjo59QtroY24CyLJIm4DVI-RMs7HV5vPWewpu6-cLF45BeYruChlgNDinkL7LrOp11ayju90RK12ZARRa_QzXAtc17BL0Jq46RL-ZIeUGQ2AK_qF0A3ROypD49wSmYhPu9I_6Vl3BbpurkQRx5GN7b7uxysD3A58K5f40c3wPB3-h0_SzfvVrZFHTZjXYmr66CO4M3j2_m2AY54NxqbqDN8Ap8ViQzDIOtkOYO1tId9piY7suY2ksca8NN5T3EEeYsYG4RZg4_mKYMWqqiLok2OOnymKQf1ukCbsb0aY1KqYxHVGODfgA4cYo-Ow3I57QJ-4HpX9Fkb723A/p.png',
+      image: 'https://i.postimg.cc/6TnRqJFH/2.png',
    },
    {
       id: 3,
@@ -46,7 +46,7 @@ export const Rick = {
          name: 'Earth (Replacement Dimension)',
          url: 'https://rickandmortyapi.com/api/location/20',
       },
-      image: 'https://previews.dropbox.com/p/thumb/AB5uv-jVRl0GSw5HfHtd4_OSw1avCv6lDSDtmF4vA-WiDzBMfh_kxkdhk_BlfOFcVvlkZJ6__P6j6NpAkmtbFbVaXgYmvIggebgDxh59ATVgo2hMEIPs7MFvqGdZqhq8dG-NboEI6Pxf11neRA66UehbHtwW6aiTCeki4EpEN2gD-cO6yy6g4y4cUn0mXudxz2iCJ5ecHDcFK10t93MTpzR4k_5S2mcpFrhsos-7Egprwc-otoGLoArx9j0I3zEOzJJ032uVJekvJUlZzWvF7jBnzTEML_wFjI-nIIvKuCXE2ZEiJ8uuJqbKJPeU1zOjMCfQ_2ImTOH_9VOJVcRtBKrDg8HovKfELexYacqXJ7kG6NzoXE32UdGsYwaDMppyFdo/p.png',
+      image: 'https://i.postimg.cc/V0qbVnjy/1.png',
    },
    {
       id: 4,
@@ -58,7 +58,7 @@ export const Rick = {
          name: 'Earth (Replacement Dimension)',
          url: 'https://rickandmortyapi.com/api/location/20',
       },
-      image: 'https://previews.dropbox.com/p/thumb/AB59_7GkmqC6HrFnd1S3oJOmoUn5ANO6dg-_rr0Nk74enIOCtxT8McF9-pbhS5F6u-OU3-qgPXUNa3ShIqr-xe0QZ9q-oHtC1hX_0bVJUGk2Ag0f6QrRiPI06wBCEOu65LbR7FaalqNmgYamtMlQUtKD68v6PPhLgNjTHhxeH_o1klf4hMw4rrCDNDxn-stXLl_gCgs_Qc5p2y-Vg93Fsb0v4gYjOqKvzdAI0_WOZ9dJkc7YK7LWe0x4UcUWyaGQqbnjq3yK1axAlJX3gZG82wUW-FAG6BaphzQ7O8ldR0sf-VGaGdx4rVf-ycsl_BmUzb8CutHEb0JcKCVZnNGt1s1j5cKxDkq8kQyADxB1yQ5g_9uBwFSTBh-S_caUB6OCbvw/p.png',
+      image: 'https://i.postimg.cc/bSctg3SH/4.png',
    },
    {
       id: 5,
@@ -70,7 +70,7 @@ export const Rick = {
          name: 'Earth (Replacement Dimension)',
          url: 'https://rickandmortyapi.com/api/location/20',
       },
-      image: 'https://previews.dropbox.com/p/thumb/AB7eHtkvA8eVweZ_28ChyYYG374k6_G6Zq-QW9Qeu8zj9Hbk6SCW8n6__VvcWWLhBYXTrE3RykC8yWn5mN9O6Lqn31Rte6tSBQ-Uh_WRQPOTU6wAGEyoTqbvdrPwzY7tHBWSgpspP3Rphwe3xqXpYfbK7asNXACgWZUvd753-mweH1JEHuW1BLg-PXkaT-Aa--2_vtMu7Juoho1ZnKn9m7TrRf5Va-UtRi0ihD4AAFw-bDiFglHLu27kymC1h037HUyVRo2X_W4rGR0AB4Drx0Tp9celbYm-NsEMVNdYT8BhpwVkVABBB57L6UcHuJS2XrGMYPjZhvyZuveK_c8OEW51ZHUrgX_KIR_Rfyda0WK9wgb1de2-Msg1uwhiexowP90/p.png',
+      image: 'https://i.postimg.cc/rDQ5T17M/3.png',
    },
 ];
 export default personajes;
