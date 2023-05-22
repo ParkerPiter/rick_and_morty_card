335e75e959ceca68844b2ace7b3daa509361aaa3
Gabriel Alejandro Leal <68304965+ParkerPiter@users.noreply.github.com>
2023-05-22 15:09:45 -0400

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -11,8 +11,11 @@ function SearchBar({onSearch}) {
 
    return (
       <div className={style.separador}>
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`${style.icon} w-6 h-6`}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+         </svg>
          <input type='search' placeholder="Search..." className={style.buscador} onChange={handleChange}/>
-         <button onClick={()=>onSearch(id)} className={style.button}>Add</button>
+         <button onClick={()=>onSearch(id)} className={style.button}>+</button>
       </div>
    );
 }
